2762b2ea64e2d025857e0d44452fd9c11098b41b
Ugyen Wangchuk <darkzephyer2004@gmail.com>
2024-03-11 14:14:33 +0600

    added one

---
+++ BrowserHistory.py
@@ -0,0 +1,37 @@
+from queue import LifoQueue
+
+backward_history = LifoQueue()
+forward_history = LifoQueue()
+current_page = None
+
+#visit function
+visitNo = int(input("Enter the number of url history: "))
+
+print("Enter URLs to visit: ")
+for i in range(visitNo): 
+    url = input("URL: ")
+    print(f"Visiting {url}")
+    backward_history.put(current_page)
+    current_page = url
+
+#Display current page
+print(f"Current page: {current_page}")
+
+#Go Backward
+while input("Do you want to go back? (yes/no): ").lower() == 'yes':
+    if not backward_history.empty():
+        forward_history.put(current_page)
+        current_page = backward_history.get()
+        print(f"Going back to {current_page}")
+else:
+    print("No previous page available.")
+
+#Go forward
+while input("Do you want to go forward? (yes/no): ").lower() == "yes":
+    if not forward_history.empty():
+        backward_history.put(current_page)
+        current_page = forward_history.get()
+        print(f"Going forward to {current_page}")
+else: 
+    print("No forward page available.")
+    
