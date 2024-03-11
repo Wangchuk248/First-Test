fe009e023dcce3ce82ec3c72b825e263496163b4
Ugyen Wangchuk <darkzephyer2004@gmail.com>
2024-03-11 13:36:29 +0600

    file 2

---
+++ classA.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+print(name)
+a = float(input("Enter Num1: "))
+b = float(input("Enter Num2: "))
+ab = a * b
+a_b = a - b 
+abab = a + b
+baba = a/b 
+print(ab,a_b,abab,baba)
+print("Provided are the results of product, difference, sum and quotient from users input. Thank you")
